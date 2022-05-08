5ad6bcc71b27f5ab4f21b046aa238e167fda6721
Sepehr <sepehr7160@gmail.com>
2022-05-08 20:11:02 +0430

    change core/init to boot add install and web into core/boot '

--- bip.php
+++ bip.php
@@ -1,5 +1,22 @@
- <pre>
  <?php
- var_dump($_GET)
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+
+//echo $_GET["data"]."\n\n";
+if(!isset($argv[0])){
+    $method = strtolower(explode('/',$_GET["data"])[0]);
+
+    if($method == 'webhook')
+    require('core/boot/init/init.php');
+    elseif($method == 'install')
+    require('core/boot/install/install.php');
+    else
+    require('core/boot/web/index.php');
+
+}else{
+    echo "Running With Console\n";
+    var_dump($argv[0]);
+}
  ?>
- </pre>
+ 
---
+++ core/boot/web/index.php
@@ -0,0 +1,2 @@
+<?php
+echo 'Bip framework is running ...';
