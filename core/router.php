4277f1394e2106507faf5c81169f0e9061dd134d
Sepehr <sepehr7160@gmail.com>
2022-05-11 17:30:57 +0430

    replace Bip with BipRouter

--- core/router.php
+++ core/router.php
@@ -2,18 +2,30 @@
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
+require __DIR__ . '/modules/Bip/BipRoute.php';
 
-require __DIR__ . '/modules/Bip/Bip.php';
-use Bip\Bip;
+use Bip\BipRoute;
 
-Bip::route('/',function(){
-    echo 'It`s work ! Bip is running ...';
+
+/*
+* You can use regex pattern in routes
+* Queries are not route
+*/
+
+#Root Route
+BipRoute::route('/','boot/web/index.php');
+
+#Webhook Route
+BipRoute::route('/webhook/?.*','boot/init/init.php');
+
+#Install Route
+BipRoute::route('/install','boot/install/install.php');
+
+
+#Help Route
+BipRoute::route('/help',function (){
+     echo 'need help ? <a href="https://github.com/sepsoh/bip"> click here ! </a>';
 });
-Bip::route('/webhook','boot/init/init.php');
 
-Bip::route('/web',function (){
-     echo 'hi from console';
 
- });
-
- 
+BipRoute::routeFlush();
