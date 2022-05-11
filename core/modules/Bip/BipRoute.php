0d2657a234bc0c717e32a01cc770fdd06084d960
Sepehr <sepehr7160@gmail.com>
2022-05-11 17:31:19 +0430

    replace Bip with BipRouter

---
+++ core/modules/Bip/BipRoute.php
@@ -0,0 +1,53 @@
+<?php
+/******************************** 
+* More on Github :
+* https://github.com/sepsoh/bip
+*
+********************************/
+namespace Bip;
+
+class BipRoute{
+    /**
+     * array of current route matches
+     * @var array
+     */
+    public static $routeMatches;
+
+    /**
+     * array of routes
+     * @var array
+     */
+    public static $routes;
+
+    /**
+     * route : main router of bip
+     *
+     * @param  string $route regex patten do not use ^$
+     * @param  mixed $call callable function or string path without __DIR__
+     */
+    public static function route(string $route , $call)
+    {
+      self::$routes[] = [$route,$call];
+    }
+    public static function routeFlush() :bool
+    {
+        //var_dump(self::$routes);
+
+        foreach(self::$routes as $rou){
+            //when bip isn`t in root path of public_html root route '/' isn`t working , this code replaces */bip/ with /
+            $path = \preg_replace('#^.*/bip/#','/',$_SERVER['REDIRECT_URL']);
+
+            if(\preg_match('#^('.$rou[0].')$#i',$path,$routeMatches)===1){
+              if(\is_callable($rou[1]))
+                  $rou[1]();
+              elseif(\is_file($rou[1]))
+                  require_once($rou[1]);
+              else
+                  throw new Exception("File : $rou[1] not found"); 
+  
+              return true;
+          }
+        }
+        return false;
+    }
+}
