0b41a240fb66dccd585c92fc3ff1083aa61d3b0e
Sepehr <sepehr7160@gmail.com>
2022-05-15 23:16:04 +0430

    telegram main driver singleton

--- core/drivers/Telegram/Telegram.php
+++ core/drivers/Telegram/Telegram.php
@@ -1,10 +1,12 @@
 <?php
 namespace Telegram;
 class Telegram{
-
-
-
-
-
+    public static $telegram ;
+    private function __construct(){}
+    static function create(){
+        if(NULL == self::$telegram)
+            self::$telegram == new Telegram();
+    }
+    
 }
 
