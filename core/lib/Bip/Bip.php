53c1d9b738b245723e302a5308a763e877421387
Sepehr <sepehr7160@gmail.com>
2022-05-31 19:38:21 +0430

    convert doc to comment

--- core/lib/Bip/Bip.php
+++ core/lib/Bip/Bip.php
@@ -1,11 +1,13 @@
 <?php
-/******************************** 
+/*
 * More on Github :
 * https://github.com/sepsoh/bip
 *
-********************************/
+*/
 namespace lib\Bip;
 
 class Bip{
-
+    public static function sayHello(){
+        echo 'hello';
+    }
 }
