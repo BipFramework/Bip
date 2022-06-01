b2caad3c1b884d4fc28bb62e9eed00320d90e489
Sepehr <sepehr7160@gmail.com>
2022-06-02 00:33:50 +0430

    first create

---
+++ core/lib/DataOperator/UserDataOperator.php
@@ -0,0 +1,23 @@
+<?php
+namespace lib\DataOperator;
+
+use lib\Database\Database;
+
+class UserDataOperator{
+    public function __construct(Database $db)
+    {
+
+    }
+    public function create(){} // create new user
+    public function isExist(){} // is user exist
+    public function getStage(){} // get user stage
+    public function setStage(){} // set user stage
+    public function set(){} // set user column
+    public function get(){} // get user column
+    public function delete(){} //delete user
+
+
+
+
+
+}
