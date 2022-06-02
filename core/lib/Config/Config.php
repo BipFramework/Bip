8836e4615345d1cabed2f7be7529ad0c5feb2d47
Sepehr <sepehr7160@gmail.com>
2022-06-02 15:25:56 +0430

    simplification and get its data from ConfigReader.php

--- core/lib/Config/Config.php
+++ core/lib/Config/Config.php
@@ -1,32 +1,33 @@
 <?php
 namespace lib\Config;
 
+use Exception;
+
 class Config
 {
-    private object $config;
-    /**
-     * save path of config file for exceptions
-     * @var string
-     */
-    private string $configPath;
-    /**
-     * @throws \Exception
-     */
+    private ConfigReader $configReader;
+
     public function __construct(string $configPath)
     {
-        $this->configPath = $configPath;
-        $this->config = (new ConfigReader($configPath))->getConfig();
+        $this->configReader = new ConfigReader($configPath);
     }
 
     /**
-     * @throws \Exception
+     * @throws Exception
      */
-    public function getByKey(string $key){
+    public function get(string $key):mixed{
 
-        if(isset($this->config->{$key}))
-            return $this->config->{$key};
+        if(isset($this->configReader->config->{$key}))
+            return $this->configReader->config->{$key};
         else
-            throw new \Exception("[$key] key not found in file ".$this->configPath.' , please read lin\Config manual ');
+            throw new Exception("[$key] key not found in file ".$this->configReader->getConfigPath().' please read lin\Config manual ');
 
+    }
+    public function set(string $key , string $value):void{
+        $this->configReader->config->{$key} = $value;
+        $this->configReader->updateConfig();
+    }
+    public function setOnlyThisTime(string $key , string $value):void{
+        $this->configReader->config->{$key} = $value;
     }
 }
