de9a6039b987495055170df88fff715463512325
Sepehr <sepehr7160@gmail.com>
2022-06-04 18:38:09 +0430

    added setup() method

--- core/lib/Bip/Bip.php
+++ core/lib/Bip/Bip.php
@@ -6,8 +6,31 @@
 */
 namespace lib\Bip;
 
+use lib\Config\Config;
+use lib\Driver\Driver;
+
 class Bip{
-    public static function sayHello(){
-        echo 'hello';
+    private static Bip $bip;
+
+    private Driver $driver;
+    private Config $config;
+
+    /**
+     * @return Bip
+     */
+    public static function getBip(): Bip
+    {
+        return self::$bip;
     }
+
+    private function __construct(){}
+    public static function setup(Driver $driver,Config $config){
+        if(self::$bip == null)
+            self::$bip = new Bip();
+
+        self::getBip()->driver = $driver;
+        self::getBip()->config = $config;
+
+    }
+
 }
