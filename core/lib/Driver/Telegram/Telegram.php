683750ce6f30e5c620efece373aa9d63578389b5
Sepehr <sepehr7160@gmail.com>
2022-06-04 18:38:57 +0430

    extends from Driver class

--- core/lib/Driver/Telegram/Telegram.php
+++ core/lib/Driver/Telegram/Telegram.php
@@ -1,14 +1,10 @@
 <?php
 namespace lib\Driver\Telegram;
 
-class Telegram{
-    private static $telegram ;
-    private function __construct(){}
-    static function create(){
-        if(NULL == self::$telegram)
-            self::$telegram == new Telegram();
-        return self::$telegram;
-    }
-    
-}
+use lib\Driver\Driver;
 
+class Telegram extends Driver
+{
+
+
+}
